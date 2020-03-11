747bd90431fe5ee0aca54ee4f6d02db17c69e715
Ryan Morr <rm.morr@gmail.com>
2020-03-11 14:40:09 -0400

    Create an avalon instance with an initial state

---
+++ src/avalon.js
@@ -0,0 +1,13 @@
+class Avalon {
+    constructor(state) {
+        this._state = Object.assign(Object.create(null), state);
+    }
+
+    state() {
+        return this._state;
+    }
+}
+
+export default function avalon(state) {
+    return new Avalon(state);
+}
--- test/specs/avalon.js
+++ test/specs/avalon.js
@@ -1,1 +1,15 @@
+import avalon from '../../src/avalon';
 
+describe('avalon', () => {
+    it('should create an initial state', () => {
+        const obj = {foo: 1, bar: 2, baz: 3};
+        const app = avalon(obj);
+
+        const state = app.state();
+
+        expect(state).to.be.a('object');
+        expect(state).to.deep.equal(obj);
+        expect(state).to.not.equal(obj);
+        expect(Object.getPrototypeOf(state)).to.equal(null);
+    });
+});
