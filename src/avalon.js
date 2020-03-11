f1de0fbe0b1efa8c518cebdd84e3db3986e946ad
Ryan Morr <rm.morr@gmail.com>
2020-03-11 15:08:52 -0400

    Move utils to own module

--- src/avalon.js
+++ src/avalon.js
@@ -1,32 +1,4 @@
-function isPlainObject(obj) {
-    if (!obj || typeof obj !== 'object') {
-        return false;
-    }
-    const prototype = Object.getPrototypeOf(obj);
-    return prototype === null || prototype === Object.getPrototypeOf({});
-}
-
-function deepFreeze(obj) {
-    if (obj == null || Object.isFrozen(obj)) {
-        return obj;
-    }
-    Object.freeze(obj);
-    if (Array.isArray(obj)) {
-        obj.forEach((item) => deepFreeze(item));
-    } else if (isPlainObject(obj)) {
-        Object.getOwnPropertyNames(obj).forEach((prop) => deepFreeze(obj[prop]));
-    }
-    return obj;
-}
-
-function createStateObject(source) {
-    return deepFreeze(Object.assign(Object.create(null), source));
-}
-
-function normalizePath(path) {
-    path = path.trim();
-    return path === '/' ? path : path.replace(/\/$/, '');
-}
+import { createStateObject, normalizePath } from './util';
 
 class Avalon {
     constructor(state = {}) {
---
+++ src/util.js
@@ -0,0 +1,31 @@
+const TRAILING_SLASH_RE = /\/$/;
+
+export function isPlainObject(obj) {
+    if (!obj || typeof obj !== 'object') {
+        return false;
+    }
+    const prototype = Object.getPrototypeOf(obj);
+    return prototype === null || prototype === Object.getPrototypeOf({});
+}
+
+export function deepFreeze(obj) {
+    if (obj == null || Object.isFrozen(obj)) {
+        return obj;
+    }
+    Object.freeze(obj);
+    if (Array.isArray(obj)) {
+        obj.forEach((item) => deepFreeze(item));
+    } else if (isPlainObject(obj)) {
+        Object.getOwnPropertyNames(obj).forEach((prop) => deepFreeze(obj[prop]));
+    }
+    return obj;
+}
+
+export function createStateObject(source) {
+    return deepFreeze(Object.assign(Object.create(null), source));
+}
+
+export function normalizePath(path) {
+    path = path.trim();
+    return path === '/' ? path : path.replace(TRAILING_SLASH_RE, '');
+}
